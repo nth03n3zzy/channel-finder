aca1f8c379b11f363ffb4dd74f9278ef40920a3d
Anthony Collins <aco1147@wgu.edu>
2023-08-31 13:57:17 -0700

    urls updated

---
+++ back-end/csv_creator.py
@@ -0,0 +1,14 @@
+import web_scaping.py
+from url_list.py import url
+import csv
+
+
+def create_csv(url):
+
+    data = [
+        ["date", 'oponnent', 'time', 'channel',]
+    ]
+
+
+for i in URL_list:
+    create_csv(URL_list[i])
---
+++ back-end/url_list.py
@@ -0,0 +1,39 @@
+class Url:
+    nba_url_list = [
+        # atlantic division
+        'https://www.espn.com/nba/team/schedule/_/name/bos/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/bkn/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/ny/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/phi/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/tor/seasontype/2',
+        # northwest divison
+        'https://www.espn.com/nba/team/schedule/_/name/den/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/min/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/okc/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/por/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/utah/seasontype/2',
+        # central divison
+        'https://www.espn.com/nba/team/schedule/_/name/chi/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/cle/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/det/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/ind/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/mil/seasontype/2',
+        # pacific divison
+        'https://www.espn.com/nba/team/schedule/_/name/gs/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/lac/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/lal/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/phx/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/sac/seasontype/2',
+        # southeast division
+        'https://www.espn.com/nba/team/schedule/_/name/atl/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/cha/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/mia/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/orl/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/wsh/seasontype/2',
+        # southwest division
+        'https://www.espn.com/nba/team/schedule/_/name/dal/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/hou/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/mem/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/no/seasontype/2',
+        'https://www.espn.com/nba/team/schedule/_/name/sa/seasontype/2'
+    ]
