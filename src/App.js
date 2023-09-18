80f8776e82aeb7b0c0961b7eff5debcb340bc2e0
Anthony Collins <aco1147@wgu.edu>
2023-09-18 11:43:54 -0700

    sda

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import MonthNavigationBar from "./components/MonthNavigationBar.js";
 import TeamNavigationBar from '/Users/daddy/Desktop/web_scraper_NBA/channel-finder/src/components/TeamNavigationBar.js';
 import NbaTeamList from "/Users/daddy/Desktop/web_scraper_NBA/channel-finder/src/Data/TeamData.js";
 import './App.css'; // Import your CSS file here
+import axios from "axios";
+
 
 const App = () => {
+  const [selectedTeam, setSelectedTeam] = useState(null);
+  const [teamData, setTeamData] = useState(null);
+
+  const handleTeamSelect = (teamAbbreviation) => {
+    setSelectedTeam(teamAbbreviation);
+
+    axios.get('http://localhost:8000/nba/schedule/${teamAbbreviation}/')
+    .then((res) => {
+      setTeamData(res.data);
+      console.log("Team Data:", res.data);
+    })
+    .catch((err) => {
+      console.error("ERROR FETCHING TEAM DATA:", err);
+    });
+  };
+
   return (
     <div className='app-container'>
       <h1 id='header' className='choose-team-header'>Choose a team</h1>
-      <TeamNavigationBar teams={NbaTeamList} />
-      <MonthNavigationBar /> 
+      <TeamNavigationBar teams={NbaTeamList} onTeamClick={handleTeamSelect} />
+      <MonthNavigationBar />
+      {/* Display team data based on the selectedTeam and teamData */}
+      {selectedTeam && teamData && (
+        <div className="team-data">
+          <h2>{selectedTeam} Schedule</h2>
+          {/* Render the team's schedule here */}
+          {/* Example: teamData.map(game => (<div key={game.id}>{game.date} - {game.opponent}</div>)) */}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/TeamNavigationBar.js
+++ src/components/TeamNavigationBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import '/Users/daddy/Desktop/web_scraper_NBA/channel-finder/src/components/TeamNavigationbar.css';
 
-const TeamNavigationBar = ({ teams }) => {
+const TeamNavigationBar = ({ teams, onTeamClick }) => {
+  const handleTeamClick = (teamAbbreviation) => {
+    if (onTeamClick) {
+      onTeamClick(teamAbbreviation);
+    }
+  };
   return (
     <div className="team-navigation-bar">
       {teams.map((team, index) => (
         <div
           key={index}
           className={`team-navigation-item team-${team.abbreviation.toLowerCase()}`}
+          onClick={()=> handleTeamClick(team.abbreviation)}
         >
           <span className="team-abbreviation">{team.abbreviation}</span>
           <span className="team-name">{team.name}</span>
