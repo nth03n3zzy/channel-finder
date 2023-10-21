c36297be2a2af2339066b05050cfc2a5d6a722e1
Anthony Collins <aco1147@wgu.edu>
2023-10-21 12:06:53 -0700

    merged new features which handle the timezone changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import MonthNavigationBar from "./components/MonthNavigationBar.js";
 import TeamNavigationBar from '/Users/daddy/Desktop/web_scraper_NBA/channel-finder/src/components/TeamNavigationBar.js';
 import NbaTeamList from "/Users/daddy/Desktop/web_scraper_NBA/channel-finder/src/Data/TeamData.js";
-import './App.css'; // Import your CSS file here
+import './App.css'; 
 import axios from "axios";  
 
 
@@ -24,13 +24,50 @@ const App = () => {
       console.error("ERROR FETCHING TEAM DATA:", err);
     });
   };
+  // get the users time zone offset once the component is mounted.
+  useEffect(() => {
+    setUserTimeZoneOffset(new Date().getTimezoneOffset());
+  }, []);
 
+  // function to convert UTC from the users local time.
+  
+  const convertToLocalTime = (utcTime) => {
+    const utcDate = new Date(utcTime);
+    const localDate = new Date(utcDate.getTime() - userTimeZoneOffset * 60000);
+
+    const localTimeStr = localDate.toLocaleTimeString(undefined, {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // Set the user's timezone
+    });
+
+    return `${localTimeStr}`;
+  };
+  // uses the game time converts the time to the local users time and converts the games date is different due to time change 
+  const convertToLocalDate = (utcTime) => {
+    const utcDate = new Date(utcTime);
+    const localDate = new Date(utcDate.getTime() - userTimeZoneOffset * 60000);
+
+    const localDateStr = localDate.toLocaleDateString(undefined, {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // Set the user's timezone
+    });
+
+    return `${localDateStr}`;
+  };
   return (
     <div className='app-container'>
       <h1 id='header' className='choose-team-header'>Choose a team</h1>
       <TeamNavigationBar teams={NbaTeamList} onTeamClick={handleTeamSelect} />
       <MonthNavigationBar />
       {/* Display team data based on the selectedTeam and teamData */}
+
+      {/* Need to add seperation for filtering out past games that have already occured,
+       placing the next upcoming game in a large block and then the rest of the games
+        occuring within the season in a block below that block. */}
       {selectedTeam && teamData && (
         <div className="team-data">
           <h2> Schedule</h2>
@@ -56,13 +93,13 @@ const App = () => {
                     {teamData.map((game, index) => (
                     <tr key = {index} className="game-data">
                       <td className="date">
-                        <span>{game.date}</span>
+                      <span>{convertToLocalDate(game.time)}</span>
                       </td>
-                      <td className="opponnent">
+                      <td className="opponent">
                         <span>{game.opponent}</span>
                       </td>
                       <td className="time">
-                        <span>{game.time}</span>
+                        <span>{convertToLocalTime(game.time)}</span>
                       </td>
                       <td className="channel">
                       <span>{game.channel.replace(/[\[\]']+/g, '')}</span>
