20335eb1f377f590653882cda27805d9ea7ccffc
Anthony Collins <aco1147@wgu.edu>
2023-10-24 15:55:57 -0700

    took out print statements add nfl csv creator

--- backend/web_scraping_code/csv_creator.py
+++ backend/web_scraping_code/csv_creator.py
@@ -2,6 +2,8 @@ import web_scraping
 from url_list import Url
 import csv
 
+# this one is for NBA  and refractoring in VS code sucks or i am dumb so it isnt tagged as such
+
 
 def create_csv(url):
     # pulls the team abbreviation from the url
@@ -11,6 +13,25 @@ def create_csv(url):
 
     data = web_scraping.get_schedule('tr', 'Table__TR Table__TR--sm Table__even',
                                      'filled Table__TR Table__TR--sm Table__even', url)
+
+    # fields names
+    field_names = ['team', 'date', 'opponent', 'time', 'channel']
+
+    # Write the data to the CSV file
+    with open(csv_file_name, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+        writer.writeheader()  # Write the header row
+        writer.writerows(data)  # Write the data rows
+
+
+def create_csv_nfl(url):
+    # pulls the team abbreviation from the url
+    csv_file_team = url.split('/')[-2]
+
+    csv_file_name = f"{csv_file_team} Schedule 2023-2024.csv"
+
+    data = web_scraping.get_schedule_nfl('tr', 'Table__TR Table__TR--sm Table__even',
+                                         'filled Table__TR Table__TR--sm Table__even', url)
 
     # fields names
     field_names = ['team', 'date', 'opponent', 'time', 'channel']
@@ -44,10 +65,11 @@ def create_csv_nhl(url):
 # LIST OF ALL TEAM URLS  TO BE ITERATED THROUGH
 urls_nba = Url.nba_url_list
 urls_nhl = Url.nhl_url_list
+urls_nfl = Url.nfl_url_list
 
 # create_csv_nhl(
 #   'https://www.espn.com/nhl/team/schedule/_/name/bos/boston-bruins')
 
 # loop creates a csv file for each team and their schedule.
-for i in range(len(urls_nhl)):
-    create_csv_nhl(urls_nhl[i])
+for i in range(len(urls_nfl)):
+    create_csv_nfl(urls_nfl[i])
