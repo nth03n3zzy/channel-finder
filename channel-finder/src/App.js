4d334259a0884fd75930fccd482b440aeed221cc
Anthony Collins <aco1147@wgu.edu>
2023-11-19 16:25:06 -0800

    refractored schedule display code to a sep file

--- channel-finder/src/App.js
+++ channel-finder/src/App.js
@@ -1,11 +1,10 @@
   import React, {useState, useEffect} from "react";
   import SportNavigationBar from "./components/SportNavigationBar.js";
   import TeamNavigationBar from './components/TeamNavigationBar.js';
-  import {NbaTeamList, NflTeamList, NhlTeamList, MlbTeamList} from "./Data/TeamData.js";
-  import { convertToLocalDateString, convertToLocalTimeString} from "./Data/DateUtils.js";
   import './App.css'; 
   import axios from "axios";  
   import TimeZoneSwitch from "./components/TimeZoneSwitch.js";
+  import {NextGameDisplay, ScheduleGameDisplay, getTeamList} from "./components/GameDisplay.js"
 
 
 
@@ -15,23 +14,9 @@
     const [userTimeZoneOffset, setUserTimeZoneOffset] = useState(0); // initialize with 0
     const [selectedTeamSchedule, setSelectedTeamSchedule] = useState ([]); // array to hold schedule to help filtering out expired games
     const [selectedSport, setSelectedSport] = useState("NBA") // team navigation bar initialize with NBA
-    const [timeGodMode, setTimeGodMode] = useState(false); // Define selectedTime
+    const [timeGodMode] = useState(false); // Define selectedTime
     const [selectedTimeZone, setSelectedTimeZone] = useState(null);
-    //switch case for when different sports are selected
-    function getTeamList(selectedSport) {
-      switch (selectedSport) {
-        case "NBA":
-          return NbaTeamList;
-        case "NFL":
-          return NflTeamList;
-        case "NHL":
-          return NhlTeamList;
-        case "MLB":
-          return MlbTeamList;
-        default:
-          return [];
-      }
-    }
+    
     
     //when a team is selected
     const handleTeamSelect = (teamAbbreviation) => {
@@ -139,95 +124,10 @@
         <TeamNavigationBar teams={getTeamList(selectedSport)} onTeamClick={handleTeamSelect} />
         <SportNavigationBar selectedSport={selectedSport} onSportSelect = {handleSportSelect} />
         <TimeZoneSwitch onTimeZoneChange={handleTimeZoneChange} timeZoneSwitchOn={timeGodMode} />
-        
-        {/* block to show the next/current game.*/}
-        {selectedTeam && teamData && (
-          <div className="team-data">
-            <h2 className="next-game-header">
-              {` ${getTeamList(selectedSport).find(team => team.abbreviation === selectedTeam)?.name} next Game`}
-            </h2>
-            <section className="next-game-section">
-              <div className="next-game-row">
-                <div className="next-game-cell next-game-date-header">
-                  <span>Date</span>
-                </div>
-                <div className="next-game-cell next-game-opponent-header">
-                  <span>Opponent</span>
-                </div>
-                <div className="next-game-cell next-game-time-header">
-                  <span>Time</span>
-                </div>
-                <div className="next-game-cell next-game-channel-header">
-                  <span>Channel</span>
-                </div>
-              </div>
-              {selectedTeamSchedule.length > 0 &&
-              <div className="next-game-row-game-info">
-                <div className="next-game-cell next-game-date">
-                  <span>{convertToLocalDateString(selectedTeamSchedule[0].time, selectedTimeZone, userTimeZoneOffset)}</span>
-                </div>
-                <div className="next-game-cell next-game-opponent">
-                  <span>{selectedTeamSchedule[0].opponent}</span>
-                </div>
-                <div className="next-game-cell next-game-time">
-                  <span>{convertToLocalTimeString(selectedTeamSchedule[0].time, selectedTimeZone, userTimeZoneOffset)}</span>
-                </div>
-                <div className="next-game-cell next-game-channel">
-                  <span>{selectedTeamSchedule[0].channel.replace(/[\[\]']+/g, '')}</span>
-                </div>
-              </div>
-              }
-            </section>
-          </div>
-        )}
-        {/* rest of team schedule displayed below */}
-        {selectedTeam && teamData && (
-          <div className="team-data">
-            <h2>{NbaTeamList.find(team => team.abbreviation === selectedTeam)?.name} upcoming Games.</h2>
-            <section className="team-schedule-section">
-              <table className="team_schedule_table">
-                <thead>
-                  <tr className="table-column-names">
-                    <th className="date-header">
-                      <span className="date-span">DATE</span>
-                    </th>
-                    <th className="opponent-header">
-                      <span>OPPONENT</span>
-                    </th>
-                    <th className="time-header">
-                      <span>TIME</span>
-                    </th>
-                    <th className="channel-header">
-                      <span>CHANNEL</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="team-schedule-table-body">
-
-                  {/* Render the upcoming games in the table below */}
-                  {selectedTeamSchedule
-                    .slice(1) // Exclude the first game (Next Game)
-                    .map((game, index) => (
-                      <tr key={index} className="game-data">
-                        <td className="date">
-                          <span>{convertToLocalDateString(game.time, selectedTimeZone, userTimeZoneOffset)}</span>
-                        </td>
-                        <td className="opponent">
-                          <span>{game.opponent}</span>
-                        </td>
-                        <td className="time">
-                          <span>{convertToLocalTimeString(game.time, selectedTimeZone, userTimeZoneOffset)}</span>
-                        </td>
-                        <td className="channel">
-                          <span>{game.channel.replace(/[\[\]']+/g, '')}</span>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            </section>
-          </div>
-        )}
+        <NextGameDisplay selectedTeam={selectedTeam} teamData={teamData} selectedTeamSchedule={selectedTeamSchedule}
+             selectedSport={selectedSport} selectedTimeZone={selectedTimeZone} />
+        <ScheduleGameDisplay selectedTeam={selectedTeam} teamData={teamData} selectedTeamSchedule={selectedTeamSchedule}
+          selectedSport={selectedSport} selectedTimeZone={selectedTimeZone} />
       </div>
     );
   };
--- channel-finder/src/components/GameDisplay.js
+++ channel-finder/src/components/GameDisplay.js
@@ -1,45 +1,138 @@
   import React from "react";
-  import getTeamList from "./App.js"
-  import { convertToLocalDateString, convertToLocalTimeString } from "./Data/DateUtils.js";
+  import "./GameDisplay.css"
+  import { convertToLocalDateString, convertToLocalTimeString } from "../Data/DateUtils.js";
+  import {NbaTeamList, NflTeamList, NhlTeamList, MlbTeamList} from "../Data/TeamData.js";
+  import userTimeZoneOffset from "../App.js"
 
-  function GameDisplay ({selectedTeam, teamData, selectedTeamSchedule, selectedSport, selectedTimeZone}){
-  {/* block to show the next/current game.*/}
-  {selectedTeam && teamData && (
-    <div className="team-data">
-      <h2 className="next-game-header">
-        {` ${getTeamList(selectedSport).find(team => team.abbreviation === selectedTeam)?.name} next Game`}
-      </h2>
-      <section className="next-game-section">
-        <div className="next-game-row">
-          <div className="next-game-cell next-game-date-header">
-            <span>Date</span>
-          </div>
-          <div className="next-game-cell next-game-opponent-header">
-            <span>Opponent</span>
-          </div>
-          <div className="next-game-cell next-game-time-header">
-            <span>Time</span>
-          </div>
-          <div className="next-game-cell next-game-channel-header">
-            <span>Channel</span>
-          </div>
+  export function getTeamList(selectedSport) {
+    switch (selectedSport) {
+      case "NBA":
+        return NbaTeamList;
+      case "NFL":
+        return NflTeamList;
+      case "NHL":
+        return NhlTeamList;
+      case "MLB":
+        return MlbTeamList;
+      default:
+        return [];
+    }
+  }
+
+  
+  function NextGameDisplay ({selectedTeam, teamData, selectedTeamSchedule, selectedSport, selectedTimeZone}){
+    const isMLBSelected = selectedSport === 'MLB';
+  /* block to show the next/current game.*/
+  if (selectedTeam && teamData) {
+    if (isMLBSelected) {
+      return (
+        <div className="team-data">
+          <h2>No MLB schedule available at this time. Please check back later.</h2>
         </div>
-        {selectedTeamSchedule.length > 0 &&
-        <div className="next-game-row-game-info">
-          <div className="next-game-cell next-game-date">
-            <span>{convertToLocalDateString(selectedTeamSchedule[0].time, selectedTimeZone)}</span>
+      );
+    } else {
+      return (
+
+      <div className="team-data">
+        <h2 className="next-game-header">
+          {` ${getTeamList(selectedSport).find(team => team.abbreviation === selectedTeam)?.name} next Game`}
+        </h2>
+        <section className="next-game-section">
+          <div className="next-game-row">
+            <div className="next-game-cell next-game-date-header">
+              <span>Date</span>
+            </div>
+            <div className="next-game-cell next-game-opponent-header">
+              <span>Opponent</span>
+            </div>
+            <div className="next-game-cell next-game-time-header">
+              <span>Time</span>
+            </div>
+            <div className="next-game-cell next-game-channel-header">
+              <span>Channel</span>
+            </div>
+            </div>
+            {selectedTeamSchedule.length > 0 && (
+            <div className="next-game-row-game-info">
+              <div className="next-game-cell next-game-date">
+                <span>{convertToLocalDateString(selectedTeamSchedule[0].time, selectedTimeZone)}</span>
+              </div>
+              <div className="next-game-cell next-game-opponent">
+                <span>{selectedTeamSchedule[0].opponent}</span>
+              </div>
+              <div className="next-game-cell next-game-time">
+                <span>{convertToLocalTimeString(selectedTeamSchedule[0].time, selectedTimeZone)}</span>
+              </div>
+              <div className="next-game-cell next-game-channel">
+                <span>{selectedTeamSchedule[0].channel.replace(/[\[\]']+/g, '')}</span>
+              </div>
+            </div>
+          )}
+            </section>
           </div>
-          <div className="next-game-cell next-game-opponent">
-            <span>{selectedTeamSchedule[0].opponent}</span>
-          </div>
-          <div className="next-game-cell next-game-time">
-            <span>{convertToLocalTimeString(selectedTeamSchedule[0].time, selectedTimeZone)}</span>
-          </div>
-          <div className="next-game-cell next-game-channel">
-            <span>{selectedTeamSchedule[0].channel.replace(/[\[\]']+/g, '')}</span>
-          </div>
-        </div>
-        }
-      </section>
-    </div>
-  )}}
+          );
+          }}}
+
+  function ScheduleGameDisplay ({selectedTeam, teamData, selectedTeamSchedule, selectedSport, selectedTimeZone}) {
+    const isMLBSelected = selectedSport === 'MLB';
+    /* block to show the next/current game.*/
+    if (selectedTeam && teamData) {
+      if (isMLBSelected) {
+        return (
+          null
+        );
+      } else {
+          return (
+            selectedTeam && teamData && (
+              <div className="team-data">
+                <h2>{NbaTeamList.find(team => team.abbreviation === selectedTeam)?.name} upcoming Games.</h2>
+                <section className="team-schedule-section">
+                  <table className="team_schedule_table">
+                    <thead>
+                      <tr className="table-column-names">
+                        <th className="date-header">
+                          <span className="date-span">DATE</span>
+                        </th>
+                        <th className="opponent-header">
+                          <span>OPPONENT</span>
+                        </th>
+                        <th className="time-header">
+                          <span>TIME</span>
+                        </th>
+                        <th className="channel-header">
+                          <span>CHANNEL</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="team-schedule-table-body">
+    
+                      {selectedTeamSchedule
+                        .slice(1) // Exclude the first game (Next Game)
+                        .map((game, index) => (
+                          <tr key={index} className="game-data">
+                            <td className="date">
+                              <span>{convertToLocalDateString(game.time, selectedTimeZone, userTimeZoneOffset)}</span>
+                            </td>
+                            <td className="opponent">
+                              <span>{game.opponent}</span>
+                            </td>
+                            <td className="time">
+                              <span>{convertToLocalTimeString(game.time, selectedTimeZone, userTimeZoneOffset)}</span>
+                            </td>
+                            <td className="channel">
+                              <span>{game.channel.replace(/[\[\]']+/g, '')}</span>
+                            </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                </section>
+              </div>
+                        )
+          );
+  }
+}
+  }
+
+
+  export  {NextGameDisplay, ScheduleGameDisplay};
